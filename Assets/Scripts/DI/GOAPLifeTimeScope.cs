7b5c73717aef697889ea0de212ab2ff374b16ff7
Daniil <pozdeev.daniil@thc.games>
2025-05-08 16:52:03 +0400

    Add envaermanet ans add scripts

---
+++ Assets/Scripts/DI/GOAPLifeTimeScope.cs
@@ -0,0 +1,14 @@
+﻿using BehaviourTree;
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    public class GoapLifeTimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<IBTDebugger, BTDebugger>(Lifetime.Singleton);
+        }
+    }
+}
